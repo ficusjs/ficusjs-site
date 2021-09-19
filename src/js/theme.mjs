024d11aeb10eb3fee2ba897fd5579974d0b42bc2
Matt Levy <ducksoupdev@gmail.com>
2021-09-19 11:03:58 +0100

    fix: logo fill color issue

--- src/js/theme.mjs
+++ src/js/theme.mjs
@@ -2,15 +2,11 @@
 
 const toggleSwitch = document.querySelector('#theme-toggle')
 const ball = document.querySelector('.fd-theme-toggle__ball')
-const logo = document.getElementById('ficus-logo')
-const darkThemeLogo = document.getElementById('ficus-logo-white')
 const currentTheme = window.localStorage.getItem('theme')
 
 function setDarkTheme () {
   toggleSwitch.checked = true
   ball.setAttribute('style', 'transform:translatex(100%);')
-  logo.style.display = 'none'
-  darkThemeLogo.style.display = 'block'
 }
 
 if (currentTheme) {
@@ -29,13 +25,9 @@ toggleSwitch.addEventListener('change', e => {
     document.documentElement.setAttribute('data-theme', 'dark')
     window.localStorage.setItem('theme', 'dark')
     ball.setAttribute('style', 'transform:translatex(100%);')
-    logo.style.display = 'none'
-    darkThemeLogo.style.display = 'block'
   } else {
     document.documentElement.setAttribute('data-theme', 'light')
     window.localStorage.setItem('theme', 'light')
     ball.setAttribute('style', 'transform:translatex(0%);')
-    logo.style.display = 'block'
-    darkThemeLogo.style.display = 'none'
   }
 })
